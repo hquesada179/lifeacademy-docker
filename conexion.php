a135e67609d4e486c102b9a0c37911c3cfe85147
hquesada179 <hquesada179@unab.edu.co>
2025-11-13 21:31:05 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -10,6 +10,4 @@ $conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-
-//echo "Connected successfully"; // (opcional para probar)
 ?>
